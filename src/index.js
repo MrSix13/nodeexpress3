3545c6f37a07bc4d5421e17352ca3a75740ae578
mrSix13 <bastiangarcesga@gmail.com>
2024-06-23 20:20:13 -0400

    as

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import pkg from 'whatsapp-web.js';
+const { Client, LocalAuth } = pkg;
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,11 +10,37 @@ app.use(express.json())
 app.use(cors());
 
 
+const client = new Client({
+    authStrategy: new LocalAuth({ dataPath: 'session' }),
+    webVersionCache: {
+      type: "remote",
+      remotePath:
+        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    },
+  });
+
+
+
 app.get('/', (req,res)=>{
-    return res.json({mensaje: "hola mundo"})
+    return res.json({mensaje: "hola mundo v2"})
 })
+
+app.get('/1', (req,res)=>{
+    return res.json({mensaje: "hola mundo- 1"})
+})
+
+
+app.get('/mensaje', (req,res)=>{
+
+
+
+})
+
+
 
 
 app.listen(port, ()=>{
     console.log('app escuchando en puerto' + port)
 })
+
+await client.initialize();
