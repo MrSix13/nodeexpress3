48f59f14bbec779566333d851ca0e17fc4e63108
mrSix13 <bastiangarcesga@gmail.com>
2024-06-24 11:56:55 -0400

    add: mongoose

--- src/index.js
+++ src/index.js
@@ -1,24 +1,108 @@
-import express from 'express';
 import cors from 'cors';
+const { Client, LocalAuth, RemoteAuth } = pkg;
 import pkg from 'whatsapp-web.js';
-const { Client, LocalAuth } = pkg;
+import qrcode from 'qrcode-terminal';
+import dotenv from 'dotenv';
+import express from 'express';
+import {mongoose} from 'mongoose';
+import { MongoStore } from 'wwebjs-mongo';
+
 
 const app = express();
-const port = process.env.PORT || 3000;
 
+dotenv.config();
 app.use(express.json())
 app.use(cors());
 
+let client       = null;
+const port       = process.env.PORT || 3000;
+//=======================MONGODB=========================//
+const MONGO_URI  = process.env.MONGODB_URI;
+
+function conectDB(){
+    mongoose.connect(MONGO_URI).then(()=>{
+      const store = new MongoStore({mongoose: mongoose});
+      if(!client){
+        client = new Client({
+          authStrategy: new RemoteAuth({
+              store: store,
+              backupSyncIntervalMs: 300000
+          }),
+          webVersionCache: {
+                  type: "remote",
+                  remotePath:
+                    "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+                },
+        });
+        client.on('qr', (qr) => {
+            qrcode.generate(qr, { small: true });
+            console.log('QR RECEIVED', qr);
+        });
+          
+        client.on('ready', () => {
+            console.log('Conectado a WhatsApp');
+        });
+      }
+      client.initialize();
+      console.log('conectado  MONGODB')
+    }).catch((error)=>console.log(error))
+};
+
+const SessionSchema = new mongoose.Schema({
+  id        : {type: String, unique:true},
+  session   : {type: Buffer},
+  qrCode    : {type: Buffer},
+  expiresAt : {type: Date}
+});
+
+const Session = mongoose.model('Session',SessionSchema);
 
 
-const client = new Client({
-    authStrategy: new LocalAuth({ dataPath: 'session' }),
-    webVersionCache: {
-      type: "remote",
-      remotePath:
-        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-    },
-  });
+class MongoRemoteAuth extends RemoteAuth{
+  constructor(store){
+    super(store);
+  }
+
+  async getSession(){
+    const sessions = await Session.find();
+    return sessions.map((session)=>({
+      id        : session.id,
+      session   : session.session,
+      qrCode    : session.qrCode,
+      expiresAt : session.expiresAt
+    }));
+  }
+
+
+  async saveSession(session){
+    const existingSession = await Session.findById(session.id);
+    if(existingSession){
+      await existingSession.updateOne(session);
+    }else{
+      const newSession = new Session(session);
+      await newSession.save();
+    }
+  }
+
+
+  async deleteSession(sessionId){
+    await Session.findByIdAndDelete(sessionId)
+  }
+}
+
+//=======================MONGODB=========================//
+
+
+
+
+// const client = new Client({
+//     authStrategy: new LocalAuth({ dataPath: 'session' }),
+//     webVersionCache: {
+//       type: "remote",
+//       remotePath:
+//         "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+//     },
+//   });
 
 
 
@@ -31,17 +115,53 @@ app.get('/1', (req,res)=>{
 })
 
 
-app.get('/mensaje', (req,res)=>{
+app.post('/enviar-mensaje', async(req,res)=>{
+  try {
+  const {numero, mensaje} = req.body;   
 
+  // if (!client) {
+  //     client = new Client({
+  //         authStrategy: new LocalAuth({ dataPath: 'session' }),
+  //         webVersionCache: {
+  //             type: "remote",
+  //             remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  //         },
+  //     });
 
+  //     // Eventos del cliente
+  //     client.on('qr', (qr) => {
+  //         qrcode.generate(qr, { small: true });
+  //         console.log('QR RECEIVED', qr);
+  //     });
 
-})
+  //     client.on('ready', () => {
+  //         console.log('Conectado a WhatsApp');
+  //     });
+
+  //     // Inicialización del cliente
+  //     await client.initialize();
+  // }
+
+      for (const phoneNumber of numero) {
+          const formattedNumber = phoneNumber + '@c.us';
+          console.log('formattedNumber:', formattedNumber)
+          await client.sendMessage(formattedNumber, mensaje);
+      }   
+     res.json({ mensaje: 'Mensajes enviados correctamente.' });
+ } catch (error) {
+  console.error('Error sending message:', error);
+  res.status(500).json({ mensaje: 'Error al enviar mensajes' });
+  }
+  
+});
+
 
 
 
 
 app.listen(port, ()=>{
     console.log('app escuchando en puerto' + port)
+    conectDB()
 })
 
-await client.initialize();
+// await client.initialize();
